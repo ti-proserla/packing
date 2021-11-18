bee9115f6bd787919b26056a8e0cdaaa02933087
diego <diegomf.mendoza@gmail.com>
2021-11-17 21:29:59 -0500

    limite de memoria para php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+ini_set('memory_limit', '256M');
 
 /*
 |--------------------------------------------------------------------------
