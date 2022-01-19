1f8475b1760609867ee9f954745dd7ac0c3ede5a
Diego Francisco Mendoza Frias <diegomf.mendoza@gmail.com>
2022-01-19 08:52:51 -0500

    nueva estructura de codigo

--- app/Http/Controllers/LoteIngresoController.php
+++ app/Http/Controllers/LoteIngresoController.php
@@ -53,10 +53,9 @@ class LoteIngresoController extends Controller
         $query="SELECT 
                 CONCAT(
                     'J',
-                    1,
                     LPAD(DAYOFYEAR(DATE_FORMAT('$fecha_cosecha', '2016-%m-%d')),3,'0'),
                     (SELECT cod_cartilla FROM cliente where id=$cliente_id)
-                    -- (SELECT cod_cartilla FROM fundo where id=$fundo_id),
+                    (SELECT cod_cartilla FROM fundo where id=$fundo_id),
                     -- (SELECT cod_cartilla FROM materia where id=$materia_id),
                     -- (SELECT cod_cartilla FROM variedad where id=$variedad_id),
                     -- SUBSTRING(YEAR('$fecha_cosecha'),-1,1),
